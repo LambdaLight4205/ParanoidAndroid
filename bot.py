0e641e9ff3ad176da635fc7d73d6842b7d2f6b53
LambdaLight4205 <lambda.light@proton.me>
2025-02-10 12:45:14 +0100

    verification des roles

--- bot.py
+++ bot.py
@@ -7,6 +7,9 @@ load_dotenv()
 print("Lancement du bot")
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
+def has_permission(user: discord.Member):
+    return any(role.name in ["Admin", "King", "Rooks"] for role in user.roles)
+
 @bot.event
 async def on_ready():
     print("Bot allumé !")
@@ -15,8 +18,7 @@ async def on_ready():
         print(f"Commands syncronisées : {len(synced)}")
     except Exception as e:
         print(e)
-    
-
+        
 @bot.event
 async def on_message(message: discord.Message):
     channel = message.channel
@@ -33,16 +35,6 @@ async def on_message(message: discord.Message):
 async def github(interaction: discord.Interaction):
     await interaction.response.send_message("Voici le lien de mon GitHub : https://github.com/LambdaLight4205")
 
-@bot.tree.command(name="warn", description="Alerter un utilisateur")
-async def warn(interaction: discord.Interaction, member: discord.Member):
-    await member.send("Tu as reçu un warn")
-    await interaction.response.send_message("Alerte envoyée")
-
-@bot.tree.command(name="ban", description="Bannir un utilisateur")
-async def ban(interaction: discord.Interaction, member: discord.Member):
-    await interaction.response.send_message(f"{member} a été banni")
-    await member.ban(reason="Tu as été banni définitivement")
-
 @bot.tree.command(name="creator", description="Informations à propos du créateur du bot")
 async def creator(interaction: discord.Interaction):
     embed = discord.Embed(
@@ -52,5 +44,23 @@ async def creator(interaction: discord.Interaction):
     )
 
     await interaction.response.send_message(embed=embed)
+
+@bot.tree.command(name="warn", description="Alerter un utilisateur")
+async def warn(interaction: discord.Interaction, member: discord.Member):
+    if not has_permission(interaction.user):
+        await interaction.response.send_message("Vous n'avez pas la permission d'utiliser cette commande", ephemeral=True)
+        return
+
+    await member.send("Tu as reçu un warn")
+    await interaction.response.send_message("Alerte envoyée", ephemeral=True)
+
+@bot.tree.command(name="ban", description="Bannir un utilisateur")
+async def ban(interaction: discord.Interaction, member: discord.Member):
+    if not has_permission(interaction.user):
+        await interaction.response.send_message("Vous n'avez pas la permission d'utiliser cette commande", ephemeral=True)
+        return
+    
+    await interaction.response.send_message(f"{member} a été banni")
+    await member.ban(reason="Tu as été banni définitivement")
 
 bot.run(os.getenv('DISCORD_TOKEN'))
